9460cf77a011e033f541df5413e0c200251cf7ce
Sharan-SS <sharanvignesh123@gmail.com>
2022-01-13 21:04:51 +0530

    Added Arrays Directory

---
+++ Arrays/evenDigits.java
@@ -0,0 +1,28 @@
+public class evenDigits {
+   public static void main(String[] args) {
+       int arr[] = {88,88782,542,23,-44};
+       int numCount = 0;
+       for(int i = 0;i<arr.length;i++){
+           if(isEven(arr[i])){
+                numCount++;
+           }
+       }
+       System.out.println(numCount);
+   } 
+   public static boolean isEven(int num){
+       int digitCount = 0;
+       if(num < 0){
+           num = num * -1;
+       }
+       else if(num == 0){
+           digitCount = 1;
+       }
+       else{
+           while(num > 0){
+               digitCount+=1;
+               num = num/10;
+           }
+       }
+       return(digitCount % 2 == 0) ? true:false;
+   }
+}
