9a876d9bc905189cad25b091a0da339964bff061
Sharan-SS <sharanvignesh123@gmail.com>
2022-01-19 23:06:14 +0530

    Added new approach

--- Arrays/peakMountain.java
+++ Arrays/peakMountain.java
@@ -1,36 +1,22 @@
 public class peakMountain {
-    public int peakIndexInMountainArray(int[] arr) {
-        return search(arr, 0, arr.length - 1);
+    public static void main(String[] args) {
+        int arr[] = {0,2,1};
+        System.out.println(arr);
     }
-
-    public int search(int arr[], int start, int end) {
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (mid == 0) {
-                if (arr[mid] > arr[mid + 1]) {
-                    return mid;
-                } else {
-                    return mid + 1;
-                }
-            } else if (mid == arr.length - 1) {
-                if (arr[mid] > arr[mid - 1]) {
-                    return mid;
-                } else {
-                    return mid - 1;
-                }
-            } else if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) {
-                return mid;
-            } else if (arr[mid] < arr[mid + 1] || arr[mid] < arr[mid - 1]) {
-                if (arr[mid - 1] > arr[mid + 1]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+        
+        while(start < end){
+            int mid = start + (end-start)/2;
+            
+            if(arr[mid] > arr[mid+1]){
+                end = mid;
             }
-
+            else{
+                start = mid+1;
+            }
         }
-        return -1;
+        return start;
     }
 }
