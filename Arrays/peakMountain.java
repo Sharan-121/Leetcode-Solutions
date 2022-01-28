86c65afd37969f4ae523ab984c0a8a70cdf7f719
Sharan S S <85382165+Sharan-121@users.noreply.github.com>
2022-01-28 21:37:05 +0530

    minor changes

--- Arrays/peakMountain.java
+++ Arrays/peakMountain.java
@@ -1,20 +1,20 @@
 public class peakMountain {
     public static void main(String[] args) {
-        int arr[] = {0,2,1};
-        System.out.println(arr);
+        int arr[] = { 0, 1, 2, 3, 4, 5, 4, 3, 2, 1 };
+        System.out.println(peakIndexInMountainArray(arr));
     }
-    public int peakIndexInMountainArray(int[] arr) {
+
+    public static int peakIndexInMountainArray(int[] arr) {
         int start = 0;
-        int end = arr.length-1;
-        
-        while(start < end){
-            int mid = start + (end-start)/2;
-            
-            if(arr[mid] > arr[mid+1]){
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] > arr[mid + 1]) {
                 end = mid;
-            }
-            else{
-                start = mid+1;
+            } else {
+                start = mid + 1;
             }
         }
         return start;
